0704ae50c9f65378f3b1753c058725a739fd222f
Qᴜɪᴇᴛᴄʀᴀꜰᴛ <145790457+Ackerman-00@users.noreply.github.com>
2026-01-27 01:07:23 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -14,6 +14,8 @@ if [ -f /etc/os-release ]; then
     sed -i 's/openSUSE Tumbleweed/Apex Linux/g' /etc/os-release
     sed -i 's/^NAME=.*/NAME="Apex Linux"/' /etc/os-release
     sed -i 's/^ID=.*/ID=apexlinux/' /etc/os-release
+    # Force Zypper to NOT install recommended packages globally
+    sed -i 's/^# solver.onlyRequires.*/solver.onlyRequires = true/' /etc/zypp/zypp.conf
     sed -i 's/^PRETTY_NAME=.*/PRETTY_NAME="Apex Linux Tumbleweed Edition"/' /etc/os-release
 fi
 
